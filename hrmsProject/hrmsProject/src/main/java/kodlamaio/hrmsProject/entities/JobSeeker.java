d3caba7fecf0f1c9a8877d1f79ffedb9342b3492
Enis Hatipoğlu <enishatipoglu.2001y@gmail.com>
2021-05-30 15:06:51 +0300

    hrmsProject
    Tekrar güncellendi.

---
+++ hrmsProject/hrmsProject/src/main/java/kodlamaio/hrmsProject/entities/JobSeeker.java
@@ -0,0 +1,91 @@
+package kodlamaio.hrmsProject.entities;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name="job=_seeker")
+@AllArgsConstructor
+@NoArgsConstructor
+public class JobSeeker extends User{
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="product_id")
+	private int userId;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "nationality_number")
+	private String nationalityNumber;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "birth_date")
+	private Date birthDate;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getNationalityNumber() {
+		return nationalityNumber;
+	}
+
+	public void setNationalityNumber(String nationalityNumber) {
+		this.nationalityNumber = nationalityNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+}
